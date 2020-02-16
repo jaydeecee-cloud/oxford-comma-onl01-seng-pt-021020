39db6029a5f097af0a05b63c27fc5269adbcf12d
Joseph Choi <jdc07@live.com>
2020-02-16 20:57:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,1 @@
 def oxford_comma(array)
-   if array.length == 1
-      array.join
-   elsif array.length == 2
-      array.join(" and ")
-   else
-      lastStr = array.pop
-      #join with "," and bring the last element back so we can do ", and" at the end
-      array.join(", ") << ", and #{lastStr}"
-      #return "#{array.join(", ")}, and #{lastStr}"
-   end
-end
-end
